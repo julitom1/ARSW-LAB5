7007059954fb3c60c039e309fe4f8e7b61b4c935
Yarit Villalobos <yarit.villalobos@mail.escuelaing.edu.co>
2021-02-18 10:04:43 -0500

    entregando avances

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -7,7 +7,12 @@ package edu.eci.arsw.blueprints.controllers;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +21,30 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
+
+@RestController
+@RequestMapping(value = "/url-raiz-recurso")
 public class BlueprintAPIController {
     
-    
-    
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<?> manejadorGetRecursoXX(){
+	    try {
+	        
+			//obtener datos que se enviarán a través del API
+	        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	    } catch (Exception ex){
+	        Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+	        return new ResponseEntity<>("Error bla bla bla",HttpStatus.NOT_FOUND);
+	    }  
+	}
     
     
 }
+
+
+
+
+    
+ 
+
 
